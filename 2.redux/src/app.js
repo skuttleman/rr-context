7042bd54137273dcc2ba836c29ca39917581a01f
James Albersheim and Jean de Klerk <pair+jalbersheim+parmejean@pivotal.io>
2016-04-19 16:08:59 -0600

    Redux fleshed out

--- 2.redux/src/app.js
+++ 2.redux/src/app.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ResultsPage from './ResultsPage'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+import ResultsPage from './components/ResultsPage'
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <ResultsPage/>,
+  <Provider store={store}>
+    <ResultsPage />
+  </Provider>,
   document.getElementById('content')
 )
---
+++ 2.redux/src/components/ResultsPage.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
+import {someAction} from '../actions/actions'
+
+const ResultsPage = React.createClass({
+  render() {
+    return (
+      <div>Hello from ResultsPage.js!</div>
+    )
+  },
+
+  componentDidMount() {
+    const {dispatch} = this.props
+    dispatch(someAction())
+  },
+})
+
+export default connect((state) => state)(ResultsPage)
