cd8696566f07c08337d40e1c46703bdb8fa597ab
James Albersheim and Jean de Klerk <pair+jalbersheim+parmejean@pivotal.io>
2016-04-19 14:23:20 -0600

    1.react with working gulp

---
+++ 1.react/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ResultsPage from './ResultsPage'
+
+ReactDOM.render(
+  <ResultsPage/>,
+  document.getElementById('content')
+)
+
+console.log('asdasdasdqweweqljkadskljdsajkasd')
